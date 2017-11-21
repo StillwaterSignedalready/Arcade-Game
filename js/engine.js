1c240c2d4deff82d4fff25d30b103d4657535445
Wayne <doublecross@yeah.net>
2017-11-21 09:25:41 +0800

    feedback0 delete jpg

--- js/engine.js
+++ js/engine.js
@@ -168,8 +168,7 @@ var Engine = (function(global) {
         'images/char-cat-girl.png',
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
-        'images/char-princess-girl.png',
-        'images/jgz.jpg'
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
